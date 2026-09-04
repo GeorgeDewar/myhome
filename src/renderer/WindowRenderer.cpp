1c3196fc12a6d969cc2cde02ab4df95d4410cf84
George Dewar <george@dewar.co.nz>
2026-09-05 00:35:47 +1200

    Draw windows quite nicely

--- src/renderer/WindowRenderer.cpp
+++ src/renderer/WindowRenderer.cpp
@@ -12,5 +12,19 @@ void WindowRenderer::renderWindow(const Wall &wall, const Opening &window) {
     liningPen.setWidth(0); // Cosmetic: always one physical screen pixel.
     painter_->setPen(liningPen);
     painter_->setBrush(Qt::NoBrush);
+
+    // Central line
     painter_->drawLine(windowStart, windowEnd);
+
+    const QVector2D halfThickness(-wall.unitDirection().y() * wall.thickness() / 2.0,
+                                  wall.unitDirection().x() * wall.thickness() / 2.0);
+
+    QPointF startA = windowStart + halfThickness.toPointF();
+    QPointF startB = windowStart - halfThickness.toPointF();
+    QPointF endA = windowEnd + halfThickness.toPointF();
+    QPointF endB = windowEnd - halfThickness.toPointF();
+
+    // Draw the two edge window lines
+    painter_->drawLine(startA, endA);
+    painter_->drawLine(endB, startB);
 }
